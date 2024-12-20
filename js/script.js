75e94bbb1a84f1a7bbfe014ced55e26a9dd4b020
marco-gulino <marcogulino98@gmail.com>
2024-12-20 14:44:08 +0100

    [fix/email] remove variable menuChoice

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
 //TRACCIA 1
 
 //VARs
-//Variabile da usare per la scelta nel menu
-let menuChoice = parseInt(
-  prompt(
-    "Benvenuto alla festa! Inserisci l'email che vuoi confrontare con quelle degl invitati"
-  )
-);
 
 //Array che conterrà le email degli invitati (impostata con alcuni valori di deafult)
 const birthdayList = ["user1email@gmail.it", "user02@gmail.it", "user3@gmail.it", "email4@gmail.com", "utentenumero5@libero.it"];
 
 //METHODS
+for(let i = 0; i < birthdayList.length; i++) {
 
+}
