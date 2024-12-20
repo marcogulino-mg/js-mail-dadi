a69df370c144cf0c53225b8df6ab4b4b332acec1
marco-gulino <marcogulino98@gmail.com>
2024-12-20 14:54:40 +0100

    [feat/email] create method that compare userEmail with birthdayList[] strings

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
 //TRACCIA 1
 
 //VARs
+//Inserisco l'email da utilizzare per il confronto con la lista
+let userEmail = prompt(
+  "Inserisci l'email da confrontare con la lista degli invitati"
+);
+
+//Variabile booleana che indica se l'email è presente o meno nella lista
+let checkEmail = false;
 
 //Array che conterrà le email degli invitati (impostata con alcuni valori di deafult)
-const birthdayList = ["user1email@gmail.it", "user02@gmail.it", "user3@gmail.it", "email4@gmail.com", "utentenumero5@libero.it"];
+const birthdayList = [
+  "user1email@gmail.it",
+  "user02@gmail.it",
+  "user3@gmail.it",
+  "email4@gmail.com",
+  "utentenumero5@libero.it",
+  "sixthuser@hotmail.com"
+];
 
 //METHODS
-for(let i = 0; i < birthdayList.length; i++) {
-
+for (let i = 0; i < birthdayList.length; i++) {
+  //Se l'email è presente nella lista allora la persona potrà partecipare alla festa
+  if (userEmail == birthdayList[i]) {
+    checkEmail = true;
+    break;
+  }
 }
