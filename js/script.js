259017cc08ad7377f09e2c2f14aa51ce52340e83
marco-gulino <marcogulino98@gmail.com>
2024-12-20 14:42:57 +0100

    [fix/email] adjust variables

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@
 //Variabile da usare per la scelta nel menu
 let menuChoice = parseInt(
   prompt(
-    "Benvenuto alla festa! Digita 1 se vuoi aggiungere l'email di un partecipante all festa. Digita 2 se vuoi controllare se una email rientra tra quelle degli invitati"
+    "Benvenuto alla festa! Inserisci l'email che vuoi confrontare con quelle degl invitati"
   )
 );
 
-//Array che conterrà le email degli invitati
-const birthdayList = [];
+//Array che conterrà le email degli invitati (impostata con alcuni valori di deafult)
+const birthdayList = ["user1email@gmail.it", "user02@gmail.it", "user3@gmail.it", "email4@gmail.com", "utentenumero5@libero.it"];
 
-
+//METHODS
 
