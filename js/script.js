f5e8a316393a47518626d30fe5fead2ae028df4a
marco-gulino <marcogulino98@gmail.com>
2024-12-20 14:35:47 +0100

    [feat/email] create variables

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//TRACCIA 1
+
+//VARs
+//Variabile da usare per la scelta nel menu
+let menuChoice = parseInt(
+  prompt(
+    "Benvenuto alla festa! Digita 1 se vuoi aggiungere l'email di un partecipante all festa. Digita 2 se vuoi controllare se una email rientra tra quelle degli invitati"
+  )
+);
+
+//Array che conterrà le email degli invitati
+const birthdayList = [];
+
+
+
